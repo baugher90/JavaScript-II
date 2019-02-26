e30bb5cf31b7253fcf1dca2bb6124f36e7a48226
baugher90 <45408433+baugher90@users.noreply.github.com>
2019-02-26 16:56:53 -0500

    challenge answered

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -82,6 +82,13 @@ console.log(largeShirts);
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+
+const totalTickets = runners.reduce((accumulator, currentValue) => {
+    return accumulator + currentValue.donation;
+  }, 0 );
+
+  ticketPriceTotal.push(totalTickets);
+
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
